ac4c71b4216dbbf2cb6c5742c0731b056e4d5c17
Sparkenstein <24642451+Sparkenstein@users.noreply.github.com>
2018-09-13 11:28:14 +0530

    restructured from demo to context menu prototype

--- background.js
+++ background.js
@@ -1,14 +1,17 @@
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
-      console.log('The color is green.');
+function setupContextMenu(){
+    chrome.contextMenus.create({
+        title:    "Bookmark here!",
+	type:     "normal",
+	id:       "12345",
+	contexts: ['selection']
     });
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-      chrome.declarativeContent.onPageChanged.addRules([{
-        conditions: [new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {hostEquals: 'developer.chrome.com'},
-        })
-        ],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-      }]);
-    });
-  });
+}
+
+
+chrome.runtime.onInstalled.addListener(function(){
+    setupContextMenu();
+});
+
+chrome.contextMenus.onClicked.addListener(function(itemData){
+    console.log("Item Data", itemData);
+});
